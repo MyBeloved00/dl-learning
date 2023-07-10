225ecc62cf3fe0a1466a24714e03a8af87cab5fd
jie <m_shijie@qq.com>
2023-07-10 18:56:52 +0800

    Changes

--- python/man.py
+++ python/man.py
@@ -1,9 +1,19 @@
-class man:
-    def __init__(self,name):
-        self.name = name
-        print("initialized")
-    def hello(self):
-        print("hello " + self.name)
+import numpy as np
 
-m = man("jack")
-m.hello() 
+
+class Man:
+    def __init__(self, name):
+        self.name = name
+
+    def hello(self):
+        print('hello ' + self.name)
+
+
+a = np.array([[1.0, 2.0, 3.0], [4.0, 2.0, 3.0]])
+b = np.array([1.0, 2.0, 3.0])
+c = np.array([1.0, 2.0])
+print(a.flatten())
+s = Man("jack")
+s.hello()
+ss = Man("sjj")
+ss.hello()
