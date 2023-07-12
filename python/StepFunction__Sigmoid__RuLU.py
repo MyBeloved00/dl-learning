5c9930f65e4ac28cb87badc9d4393476a2263bb6
jie <m_shijie@qq.com>
2023-07-12 16:50:12 +0800

    submit 3 function of activation function

---
+++ python/StepFunction__Sigmoid__RuLU.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def step_function(x):
+    y = x > 0
+    return y.astype("int")
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+x = np.arange(-5, 5, 0.1)
+y1 = step_function(x)
+y2 = sigmoid(x)
+y3 = relu(x)
+plt.plot(x, y1, label="step_function")
+plt.plot(x, y2, linestyle="--", label="sigmoid")
+plt.plot(x, y3, linestyle="solid", label="relu")
+plt.ylim(-0.1, 2)
+plt.legend()
+plt.show()
--- python/test.py
+++ python/test.py
@@ -1,8 +1,24 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as im
 import numpy as np
 
-img = im.imread("razer-blade-14-Synthesize_2560x1440.jpg")
-plt.imshow(img)
 
-plt.show()
+def relu(x):
+    return np.maximum(0, x)
+
+
+def layer_compute(x, w):
+    a = np.dot(x, w)
+    return relu(a + b)
+
+
+x = np.array([1, 5, 2])  # 第一个输入为bias的系数
+b = np.array([0.1, 0.2, 0.3])
+w = np.array([b, [1, 3, 5], [2, 4, 6]])
+w2 = np.array([[2, 3, 4], [3, 4, 5], [2, 5, 7]])
+w3 = np.array([[5, 6], [3, 4], [4, 6]])
+layer1 = layer_compute(x, w)
+print(layer1)
+layer2 = layer_compute(layer1, w2)
+print(layer2)
+print(w3.shape)
+y = layer_compute(layer2, w3)
+print(y)
