8f76b1d69822dc77ad20f20e49c873714a4ee33a
MSJ's PC <m_shijie@qq.com>
2023-07-21 15:51:03 +0800

    add the numerical_gradient

--- python/diff_compute.py
+++ python/diff_compute.py
@@ -6,25 +6,45 @@ def function_1(x):
     return 0.01 * x ** 2 + 0.1 * x
 
 
-def numerial_diff(f, x):
+def function_2(x):
+    return np.sum(x ** 2)
+
+
+def numerical_diff(f, x):
     h = 1e-4
     return (f(x + h) - f(x - h)) / (2 * h)
 
 
-def slope(f, x, point):#生成一条f在x范围内，关于point点的切线
-    k = numerial_diff(f, point)
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+    for i in range(x.size):
+        temp = x[i]
+
+        x[i] = temp + h
+        diffval1 = f(x)
+
+        x[i] = temp - h
+        diffval2 = f(x)
+
+        grad[i] = (diffval1 - diffval2) / (2 * h)
+        x[i] = temp
+    return grad
+
+
+def slope(f, x, point):  # 生成一条f在x范围内，关于point点的切线
+    k = numerical_diff(f, point)
     b = f(point) - k * point
     return k * x + b
 
-
 # show the image of function_1
-x = np.arange(0, 20, 0.1)
-y = function_1(x)
-k1 = numerial_diff(function_1, 5)
-plt.xlabel("x")
-plt.ylabel("y(x)")
-plt.plot(x, y, label="Function1")
-plt.plot(x, slope(function_1, x, 5), label="slope of Function1 in 5")
-plt.plot(x, slope(function_1, x, 10), label="slope of Function1 in 10")
-plt.legend()
-plt.show()
+# x = np.arange(0, 20, 0.1)
+# y = function_1(x)
+# k1 = numerical_diff(function_1, 5)
+# plt.xlabel("x")
+# plt.ylabel("y(x)")
+# plt.plot(x, y, label="Function1")
+# plt.plot(x, slope(function_1, x, 5), label="slope of Function1 in 5")
+# plt.plot(x, slope(function_1, x, 10), label="slope of Function1 in 10")
+# plt.legend()
+# plt.show()
